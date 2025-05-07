93a98da0b3c1806cd26d5df35698da289b086d9a
Sagaofficialhub <169112788+sagazon@users.noreply.github.com>
2025-05-07 19:02:55 +0530

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // This program calculates the area of a circle using a fixed radius
+
+    float pi = 3.14; 
+    int r = 2;
+    float c = pi * r * r;
+
+    printf("The value of c is %f", c);
+
+    return 0;
+}
