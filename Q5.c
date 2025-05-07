9965ecb3ad91bd1b16a0b63cff8cfafa1f764bbd
Sagaofficialhub <169112788+sagazon@users.noreply.github.com>
2025-05-07 19:08:23 +0530

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // This program performs a mathematical expression with multiplication and addition
+
+    int x = 8, y = 2, z = 0, q;
+    q = x * y + z;
+
+    printf("The value of q is %d", q);
+
+    return 0;
+}
