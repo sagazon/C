c1612b1f2509a4a3a5cca38bf69f17535498ce18
Sagaofficialhub <169112788+sagazon@users.noreply.github.com>
2025-05-07 19:00:05 +0530

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // This program adds two predefined integers and prints the result
+
+    int x = 10, y = 22, z;
+    z = x + y;
+
+    printf("The value of z is %d", z);
+
+    return 0;
+}
