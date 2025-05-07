4104ecd7e09b505aded668bdf715e584370e1267
Sagaofficialhub <169112788+sagazon@users.noreply.github.com>
2025-05-07 19:04:52 +0530

    Create Q4.c

---
+++ Q4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // This program performs integer division and modulus operation
+
+    int a = 10, b = 3, c, d;
+    c = a / b;
+    d = a % b;
+
+    printf("The value of a/b is %d\nThe value of a%%b is %d", c, d);
+
+    return 0;
+}
