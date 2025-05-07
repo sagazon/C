8f25d2dc53130abf53013e41e7ee29e73064030f
Sagaofficialhub <169112788+sagazon@users.noreply.github.com>
2025-05-07 19:39:35 +0530

    Create Q9.c

---
+++ Q9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Function to calculate and print the square of a
+void square() {
+    int a = 5;
+    int b = a * a;  // Calculating the square
+    printf("The square of a is = %d", b);  // Printing the result
+}
+
+int main() {
+    square();  // Calling the square function
+    return 0;
+}
