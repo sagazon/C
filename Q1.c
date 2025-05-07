1a94fe3b269929c9708a37f98be27c79e35cdec6
Sagaofficialhub <169112788+sagazon@users.noreply.github.com>
2025-05-07 18:47:23 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // This program adds two integers and prints the result
+
+    int a = 5, b = 7;
+    int c = a + b;
+
+    printf("The value of a + b is equal to %d", c);
+
+    return 0;
+}
